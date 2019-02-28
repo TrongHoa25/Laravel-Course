39952d947452ffb13732e242d7b296bd56e78899
TrongHoa25 <nhokteti2509@gmail.com>
2019-02-28 15:38:07 +0700

    finish lesson-controller

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,20 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
+Route::get('/',function(){
     return view('welcome');
 });
+
+Route::get('foo', function () {
+    return 'Hello World';
+});
+Route::get('posts/{post}/comments/{comment}',function($postId, $commentId){
+
+});
+
+// Route::get('user/{age}', function ($age) {
+//         return 'age = '.$age;
+//    })->middleware('checkage');
+
+// Route::get('user/list','UserController@show');
+Route::resource('users','UserController');
